5b7843cd46bff783b69bc15f662a3d19d99dde36
Nicholas Joseph Rosato <nrosato@cya.cs.umb.edu>
2014-05-08 16:01:58 -0400

    added dataentry.rb for trux

---
+++ doc/dataentry.rb
@@ -0,0 +1,140 @@
+Album.create(id:1,title: "Genius of Modern Music: Volume 1", format: "7",released:(1950,1,1),credits:"",notes"",cover:"",artist_id:1);
+Artist.create(id:1,name:"Benny Goodman");
+Track.create(album_id:1,position: 1,title:"Don't Be That Way");
+
+Track.create(album_id:1,position: 2,title:"One O'Clock Jump");
+Track.create(album_id:1,position: 3,title:"Sensation Rag");
+Track.create(album_id:1,position: 4,title:"I'm Coming Virginia");
+Track.create(album_id:1,position: 5,title:"When My Baby Smiles at Me");
+Track.create(album_id:1,position: 6,title:"Shine");
+Track.create(album_id:1,position: 7,title:"Blue Reverie");
+Track.create(album_id:1,position: 8,title:"Life Goes to a Party");
+Track.create(album_id:1,position: 9,title:"Honeysuckle Rose");
+Track.create(album_id:1,position: 10,title:"Body and Soul");
+Track.create(album_id:1,position: 11,title:"Avalon");
+Track.create(album_id:1,position: 12,title:"The Man I Love");
+Track.create(album_id:1,position: 13,title:"I Got Rhythm");
+Track.create(album_id:1,position: 14,title:"Blue Skies");
+Track.create(album_id:1,position: 15,title:"Loch Lomond");
+Track.create(album_id:1,position: 16,title:"Blue Room");
+Track.create(album_id:1,position: 17,title:"Swingtime in the Rockies");
+Track.create(album_id:1,position: 18,title:"Bei Mir Bist du Schoen");
+Track.create(album_id:1,position: 19,title:"China Boy");
+Track.create(album_id:1,position: 20,title:"Stompin' at the Savoy");
+Track.create(album_id:1,position: 21,title:"Dizzy Spells");
+Track.create(album_id:1,position: 22,title:"Sing Sing Sing"); 
+Track.create(album_id:1,position: 23,title:"Big John's Special");
+Artist.create(id:2,name:"Thelonious Monk");
+Album.create(id:1,title: "Genius of Modern Music: Volume 1", format: "7",released:(1951,1,1),credits:"",notes"",cover:"",artist_id:2);
+Track.create(album_id:2,position: 1,title:"Round Midnight (as 'Round About Midnight')");
+Track.create(album_id:2,position: 2,title:"Off Minor");
+Track.create(album_id:2,position: 3,title:"Ruby my Dear");
+Track.create(album_id:2,position: 4,title:"I Mean You");
+Track.create(album_id:2,position: 5,title:"Thelonious");
+Track.create(album_id:2,position: 6,title:"Epistrophy");
+Track.create(album_id:2,position: 7,title:"Well You Needn't");
+Track.create(album_id:2,position: 8,title:"Misterioso");
+Artist.create(id:3,name:"Peggy Lee");
+Album.create(id:3,title: "Black Coffee by Peggy Lee (1953)", format: "7",released:(1953,1,1),credits:"",notes"",cover:"",artist_id:3);
+Track.create(album_id:3,position: 1,title:"Black Coffee");
+
+Track.create(album_id:3,position: 2,title:"I've Got You Under My Skin");
+Track.create(album_id:3,position: 3,title:"Easy Living");
+Track.create(album_id:3,position: 4,title:"My Heart Belongs To Daddy")
+Track.create(album_id:3,position: 5,title:"It Ain't Necessarily So");
+Track.create(album_id:3,position: 6,title:"Gee Baby (Ain't I Good To You)");
+Track.create(album_id:3,position: 7,title:"Woman Alone With The Blues");
+Track.create(album_id:3,position: 8,title:"I Didn't Know What Time It Was");
+Track.create(album_id:3,position: 9,title:"When The World Was Young");
+Track.create(album_id:3,position: 10,title:"Love Me Or Leave Me");
+Track.create(album_id:3,position: 11,title:"You're My Thrill");
+Track.create(album_id:3,position: 12,title:"There's A Small Hotel");
+Artist.create(id:4,name:"Sarah Vaughan");
+Album.create(id:4,title: "Sarah Vaughan", format: "7",released:(1954,1,1),credits:"",notes"",cover:"",artist_id:4);
+Track.create(album_id:4,position: 1,title:"Lullaby Of Birdland");
+Track.create(album_id:4,position: 2,title:"April In Paris");
+Track.create(album_id:4,position: 3,title:"He's My Guy");
+Track.create(album_id:4,position: 4,title:"Jim");
+Track.create(album_id:4,position: 5,title:"You're Not The Kind");
+Track.create(album_id:4,position: 6,title:"Embraceable You");
+Track.create(album_id:4,position: 7,title:"I'm Glad There Is You");
+Track.create(album_id:4,position: 8,title:"September Song");
+Track.create(album_id:4,position: 9,title:"It's Crazy");
+Artist.create(id:5,name:"Elvis Presley");
+Album.create(id:5,title: "Elvis Presley", format: "7",released:(1956,1,1),credits:"",notes"",cover:"",artist_id:5);
+Track.create(album_id:5,position: 1,title:"Blue Suede Shoes");
+Track.create(album_id:5,position: 2,title:"I'm Counting On You");
+Track.create(album_id:5,position: 3,title:"I Got A Woman");
+Track.create(album_id:5,position: 4,title:"One-Sided Love Affair");
+Track.create(album_id:5,position: 5,title:"I Love You Because");
+Track.create(album_id:5,position: 6,title:"Just Because");
+Track.create(album_id:5,position: 7,title:"Tutti Frutti");
+Track.create(album_id:5,position: 8,title:"Tryin' To Get To You");
+Track.create(album_id:5,position: 9,title:"I'm Gonna Sit Right Down And Cry (Over You)");
+User.create(pass:"asdkKl12",username:"poper12",address:1);
+User.create(pass:"asdasPl09",username:"musiclover12",address:2);
+User.create(pass:"asdasdO123",username:"funny12",address:3);
+User.create(pass:"asdasO12sadA",username:"jonathan1",address:4);
+User.create(pass:"poal12POl",username:"bob123",address:5);
+User.create(pass:"as12Lso",username:"loving12",address:6);
+Item(type:"",format:"",demand:1,available:true,album_id:1);
+Item(type:"",format:"",demand:1,available:true,album_id:2);
+Item(type:"",format:"",demand:1,available:true,album_id:3);
+Item(type:"",format:"",demand:1,available:true,album_id:4);
+Item(type:"",format:"",demand:1,available:true,album_id:5);
+Transaction(paymentmethod_id:1,item_id:1,user_id:1);
+Transaction(paymentmethod_id:2,item_id:2,user_id:2);
+Transaction(paymentmethod_id:3,item_id:3,user_id:3);
+Transaction(paymentmethod_id:4,item_id:4,user_id:4);
+Transaction(paymentmethod_id:5,item_id:5,user_id:5);
+PaymentMethod.create(type:"MasterCard",identifier:"5210841851706277",validation"933",expires:(2014,2,1),address_id:1);
+PaymentMethod.create(type:"MasterCard",identifier:"5117093743377206",validation"123",expires:(2018,9,1),address_id:2);
+
+PaymentMethod.create(type:"MasterCard",identifier:"5187558831785812",validation"029",expires:(2016,2,1),address_id:3);
+
+PaymentMethod.create(type:"Visa",identifier:"4565980711382293",validation"983",expires:(2017,3,1),address_id:4);
+
+PaymentMethod.create(type:"Visa",identifier:"4615603904434875",validation"123",expires:(2015,2,1),address_id:5);
+Address.create(name:"Berel Winer",street:"553 Main Steet",city:"Brookyln",state:"NY",country:"USA");
+Address.create(name:"Rachel Miller",street:"81 Dale Street,city:"Waltham",state:"MA",country:"USA");
+Address.create(name:"Mendy Fisher",street:"32 Depot Street",city:"Sharon",state:"MA",country:"USA");
+Address.create(name:"Jonathan Goldman",street:"452 Main Street",city:"Queens",state:"NY",country:"USA");
+Address.create(name:"John Mad",street:"29 Beacon Street",city:"Boston",state:"MA",country:"USA");
+Rightsholder.create(name:"Elvis Preasley",rate:1.1,address_id:7)
+Rightsholder.create(name:"Sarah Vaughan",rate:1.1,address_id:8)
+Rightsholder.create(name:"Peggy Lee",rate:1.1,address_id:9)
+Rightsholder.create(name:"Benny Goodman",rate:1.1,address_id:10)
+Rightsholder.create(name:"Thelonious Monk",rate:1.1,address_id:11)
+Address.create(name:"Elvis Preasley");
+Address.create(name:"Sarah Vaughan");
+Address.create(name:"Peggy Lee");
+Address.create(name:"Benny Goodman");
+Address.create(name:"Thelonious Monk");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
