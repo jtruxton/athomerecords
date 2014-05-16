e572dcbe0d9f2b11a89d6d72a8e3b2a74208e9e5
jtruxton <yonatan13200@yahoo.com>
2014-05-16 11:20:55 -0400

    Update railstables.rb

--- doc/railstables.rb
+++ doc/railstables.rb
@@ -7,32 +7,35 @@ class CreateAlbums < ActiveRecord::Migration
       t.string :credits
       t.string :notes
       t.string :cover
-      t.int :artist_id
- 
+      t.integer: genre_id 
       t.timestamps
     end
   end
 end
 
-class CreateArtits < ActiveRecord::Migration
+class CreateArtists < ActiveRecord::Migration
   def change
     create_table :artists do |t|
       t.string :name
       t.string :bio
-      t.int :album_id
  
       t.timestamps
     end
   end
 end
-
+class CreateProduces < ActiveRecord::Migration
+  def change
+     create_table: produces do |t|
+	t.belongs_to :artist
+ 	t.belongs_to :album
+        t.belongs_to: label
 class CreateTracks < ActiveRecord::Migration
   def change
     create_table :tracks do |t|
-      t.int :position
+      t.belongs_to : album
+      t.integer :position
       t.string :title
       t.string :details
-      t.int :album_id
  
       t.timestamps
     end
@@ -53,7 +56,7 @@ class CreateLabels < ActiveRecord::Migration
   def change
     create_table :lables do |t|
       t.string :name
-      t.int :rightsholders_id
+      t.integer :rightsholders_id
  
       t.timestamps
     end
@@ -63,33 +66,25 @@ end
 class CreateRightholders < ActiveRecord::Migration
   def change
     create_table :rightsholders do |t|
+      t.belongs_to :label
       t.string :name
-      t.double :rate
-      t.int :address_id
-      i.int :label_id
+      t.decimal :rate
+      t.integer :address_id
+ 
  
       t.timestamps
     end
   end
 end
 
-class CreateUsers < ActiveRecord::Migration
-  def change
-    create_table :users do |t|
-      t.string :username
-      t.string :login_id
- 
-      t.timestamps
-    end
-  end
-end
 
 class CreateItems < ActiveRecord::Migration
   def change
     create_table :items do |t|
       t.string :type
       t.string :format
-      t.int :demand
+      t.integer :demand
+      t.integer :stock	
       t.boolean :available
  
       t.timestamps
@@ -100,9 +95,9 @@ end
 class CreateTransactions < ActiveRecord::Migration
   def change
     create_table :transactions do |t|
-      t.int :paymentmethod_id
-      t.int :item_id
-      i.int :user_id
+      t.integer :paymentmethod_id
+      t.integer :item_id
+      t.integer :user_id
  
       t.timestamps
     end
@@ -112,12 +107,13 @@ end
 class CreateAdresses < ActiveRecord::Migration
   def change
     create_table :adresses do |t|
+      t.belongs_to: user
       t.string :name
       t.string :street1
       t.string :street2
       t.string :city
       t.string :state
-      t.int :zipcode
+      t.integer :zipcode
       t.string :country
       
       t.timestamps
@@ -128,11 +124,12 @@ end
 class CreatePaymentmethods < ActiveRecord::Migration
   def change
     create_table :payments do |t|
+      t.belongs_to:user
       t.string :type
-      t.int :identifier
+      t.integer :identifier
       t.string :validation
       t.date :expires
-      t.int :address_id
+      t.integer :address_id
  
       t.timestamps
     end
